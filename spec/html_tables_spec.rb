9cef1d705d3fa9dbf989a423aace08ed31f5ddaa
steven <bbtrombone@sbcglobal.net>
2020-04-28 23:44:03 +0000

    Automatically backed up by Learn

--- spec/html_tables_spec.rb
+++ spec/html_tables_spec.rb
@@ -1,12 +1,11 @@
 require 'pry'
-
+ntals/fswd-prework-2-01-fswd-
 RSpec.describe 'index.html' do
   it 'contains a <table> with five rows' do
     table = parsed_html.search('table').first
     expect(table).to_not be_nil, "The main <table> tag is missing!"
     expect(html_file_contents).to include('</table>'), "Don't forget to include a closing </table>"
-
-
+https://learn.co/tracks/fswd-prework-2-01/fswd-prework-2-01-fswd-prework/fswd-prework-2-01-fswd-prework-html-fundameprework-html-fundamentals-html-tables-lab
     children = table.children.select {|child| child.name == "tr"}
     expect(children.length).to be == 5, "Your <table> tag needs five <tr> tags, one for each row"
 
